dc613c6a81ac4b0cc131d13186a21311b7da74e0
Ilie <oanceailie99@gmail.com>
2026-07-09 18:20:05 +0300

    hero init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   fonts: [
     {
       provider: fontProviders.fontsource(),
       name: 'Roboto',
       cssVariable: '--font-roboto',
       styles: ['normal'],
-      weights: [400, 700],
+      weights: [400, 700, 900],
     },
   ],
   vite: {
